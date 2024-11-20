6d95ea1081a7c1c35e684c689a826e924c9c43ce
Dennis <itgenius1004@gmail.com>
2024-11-20 15:56:43 +0900

    ok

--- user-service/src/main/java/com/example/user_service/controller/UserController.java
+++ user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.user_service.model.User;
 import com.example.user_service.service.UserServiceImpl;
 
 @RestController // @Controller
+@RequestMapping("/api/users")
 public class UserController {
     @Autowired
     private UserServiceImpl userServiceImpl;
