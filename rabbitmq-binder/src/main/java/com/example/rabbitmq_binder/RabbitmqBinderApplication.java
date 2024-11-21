299623470008772331a77469a1684be688097e37
Dennis <itgenius1004@gmail.com>
2024-11-21 13:53:30 +0900

    ok

---
+++ rabbitmq-binder/src/main/java/com/example/rabbitmq_binder/RabbitmqBinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq_binder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqBinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqBinderApplication.class, args);
+	}
+
+}
