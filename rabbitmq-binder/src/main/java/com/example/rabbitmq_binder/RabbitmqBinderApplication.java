7ebe6a722a7a9fe01769efe13064426e6e1f451b
Dennis <itgenius1004@gmail.com>
2024-11-22 09:34:27 +0900

    ok

---
+++ rabbitmq-binder/src/main/java/com/example/rabbitmq_binder/RabbitmqBinderApplication.java
@@ -0,0 +1,34 @@
+package com.example.rabbitmq_binder;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RabbitmqBinderApplication {
+	private static final Logger log = LoggerFactory.getLogger(RabbitmqBinderApplication.class);	
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqBinderApplication.class, args);
+	}
+
+	@Bean
+	public Supplier<String> mySupplierSource() {
+		return () -> {
+			String message = "www.pkslow.com";
+			log.info("Sending value: " + message);
+			return message;
+		};
+	}
+
+	@Bean
+	public Consumer<String> myConsumerSink() {
+		return message -> {
+			log.info("Received message " + message);
+		};
+	}
+}
---
+++ rabbitmq-binder/src/test/java/com/example/rabbitmq_binder/RabbitmqBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq_binder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqBinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
